71ddd5fa26b80346ac8792858c1158de5e275357
unknown <sabrinsultana6@gmail.com>
2018-02-19 03:01:33 +0600

    fix carousel

--- custom.js
+++ custom.js
@@ -1,6 +1,10 @@
-$('.img-slider').owlCarousel({
-    loop:true,
-    nav:true,
-    items:3,
-    dots:true
-})
+$(document).ready(function(){
+	$('.img-slider').owlCarousel({
+	    loop:true,
+	    nav:true,
+	    items:3,
+	    dots:false,
+	    navText:['<i class="fa fa-lg fa-arrow-circle-left"></i>', '<i class="fa fa-lg fa-arrow-circle-right"></i>']
+	})
+});
+
